c0a99ac8004beb7c45634e37e35ed5aa0bad1e38
pc3p0 <jm_tech@zohomail.com>
2022-03-15 16:04:46 -0400

    Added basic logic

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:git_viewer/model/git_repo.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MyApp());
@@ -7,109 +12,138 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Git Viewer',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'GitViewer'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
-
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  final _textController = TextEditingController();
+  bool _hasResults = false;
+  List<GitRepo> _repos = [];
 
-  void _incrementCounter() {
+  void _updateResults(bool hasresults, List<GitRepo> repos) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _hasResults = hasresults;
+      _repos = repos;
     });
+  }
+
+  final _defaultBody = Column(
+    mainAxisAlignment: MainAxisAlignment.center,
+    crossAxisAlignment: CrossAxisAlignment.stretch,
+    children: const <Widget>[
+      FlutterLogo(size: 60),
+      SizedBox(height: 24),
+      Text(
+        'Enter a GitHub username to view all public repositories.',
+        textAlign: TextAlign.center,
+      ),
+    ],
+  );
+
+  Widget _buildResults() {
+    return ListView.separated(
+      shrinkWrap: true,
+      itemCount: _repos.length,
+      separatorBuilder: (_, idx) => const Divider(),
+      itemBuilder: (_, idx) {
+        var repo = _repos[idx];
+        return ListTile(
+          leading: repo.owner?.avatarUrl != null
+              ? CircleAvatar(
+                  backgroundImage: NetworkImage(repo.owner?.avatarUrl ?? ''),
+                )
+              : null,
+          title: Text(
+            _repos[idx].name ?? 'Repository Name',
+            style: const TextStyle(fontWeight: FontWeight.w500),
+          ),
+          subtitle: Text(
+            repo.description ?? 'Repository description',
+            // style: TextStyle(fontSize: 12),
+          ),
+        );
+      },
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    var theme = Theme.of(context);
+
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: TextFormField(
+            controller: _textController,
+            textInputAction: TextInputAction.done,
+            cursorColor: theme.colorScheme.onPrimary,
+            style: TextStyle(color: theme.colorScheme.onPrimary),
+            decoration: InputDecoration(
+              hintText: 'GitHub username',
+              hintStyle: TextStyle(color: theme.colorScheme.onPrimary),
+              prefixIcon: Icon(
+                Icons.search,
+                color: theme.colorScheme.onPrimary,
+              ),
+              isDense: true,
+              contentPadding: EdgeInsets.zero,
+              border: const OutlineInputBorder(
+                borderSide: BorderSide.none,
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+            onFieldSubmitted: (val) async =>
+                await _fetchPublicRepositories(val),
+          ),
+        ),
+        body: AnimatedCrossFade(
+          firstChild: _defaultBody,
+          secondChild: _buildResults(),
+          crossFadeState: _hasResults
+              ? CrossFadeState.showSecond
+              : CrossFadeState.showFirst,
+          duration: const Duration(milliseconds: 300),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  Future<void> _fetchPublicRepositories(String userInput) async {
+    // var username = _textController.text.trim();
+    var url = Uri.https('api.github.com', '/users/$userInput/repos');
+    try {
+      var response = await http.get(url);
+      if (response.statusCode == 200) {
+        var jsonConvert = jsonDecode(response.body) as List<dynamic>;
+        // var gitRepo = gitRepoFromMap(response.body);
+        // var gitRepo = GitRepo.fromJson(jsonConvert[0]);
+        var repos = jsonConvert.map(((e) => GitRepo.fromJson(e))).toList();
+        _updateResults(true, repos);
+      }
+    } catch (e, _) {
+      log('Eror: ' + e.toString());
+    }
+  }
+
+  @override
+  void dispose() {
+    _textController.dispose();
+    super.dispose();
   }
 }
